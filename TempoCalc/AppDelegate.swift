2e7530e029e67f61502b649afe7bf40adf5e99b4
Josh Smith <registerednerd@gmail.com>
2015-10-11 07:16:19 -0500

    Initial Commit

---
+++ TempoCalc/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TempoCalc
+//
+//  Created by Josh Smith on 10/11/15.
+//  Copyright © 2015 Grace Evangelical Church. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TempoCalc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TempoCalc
+//
+//  Created by Josh Smith on 10/11/15.
+//  Copyright © 2015 Grace Evangelical Church. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
